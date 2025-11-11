50c0c0567ead74196ca522b09cd746fd51c6999f
ItsSleepy <isaacclaudec@gmail.com>
2025-11-11 10:38:46 +0000

    Created contact model

--- database/migrations/2025_11_10_130057_create_contacts.php
+++ database/migrations/2025_11_10_130057_create_contacts.php
@@ -15,10 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email');
             $table->string('phone')->nullable();
+            $table->string('email');
             $table->string('address');
-            $table->foreignId('company_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
